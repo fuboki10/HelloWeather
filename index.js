d81beacb0d68f8f0ab61c17aa5a5b285cc8898c6
fuboki10 <abdelrahman.tarek1910@gmail.com>
2020-08-26 21:59:23 +0200

    Add click listener to change units

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 const API_KEY = '3056be121e8647e4248094ac0a7cf18d';
 
+const kelvinToCelsius = (degree) => {
+    return degree - 273.15;
+};
+
+const kelvinToFahrenheit = (degree) => {
+    return degree * 1.8 - 459.67;
+};
+
 $(() => {
     const temperatureDescription = $('.temperature-description');
     const temperatureDegree = $('.temperature-degree');
     const location = $('.location-timezone');
     const weatherIcon = $('#weather-icon');
+    const degreeSection = $('.degree-section');
+    const unit = $('#unit');
+
+    let temperature;
     
     if ("geolocation" in navigator){ //check Geolocation available 
       navigator.geolocation.getCurrentPosition(position => {
@@ -20,17 +32,32 @@ $(() => {
                 return res.json();
             })
             .then(data => {
-                const temperature = data.main.temp;
+                temperature = data.main.temp;
                 const {description, icon} = data.weather[0];
                 const country = data.sys.country;
                 const city = data.name;
 
-                temperatureDegree[0].innerHTML = temperature;
+                temperatureDegree[0].innerHTML = Math.floor(kelvinToCelsius(temperature));
                 temperatureDescription[0].innerHTML = description;
                 location[0].innerHTML = `${country}/${city}`;
 
-                console.log(weatherIcon);
                 weatherIcon.append(`<img src="http://openweathermap.org/img/wn/${icon}@2x.png"></img>`);
+            });
+
+            // click listener
+            degreeSection.on('click', () => {
+                if (unit[0].innerHTML == 'C') { // convert to F
+                    unit[0].innerHTML = 'F';
+                    temperatureDegree[0].innerHTML = Math.floor(kelvinToFahrenheit(temperature));
+                }
+                else if (unit[0].innerHTML == 'F') { // convert to K
+                    unit[0].innerHTML = 'K';
+                    temperatureDegree[0].innerHTML = Math.floor(temperature);
+                }
+                else if (unit[0].innerHTML == 'K') { // convert to C
+                    unit[0].innerHTML = 'C';
+                    temperatureDegree[0].innerHTML = Math.floor(kelvinToCelsius(temperature));
+                }
             });
       }); 
     } else{
