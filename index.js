a3586f4d886dfe94a79f8a6e1ff89b0010285821
fuboki10 <abdelrahman.tarek1910@gmail.com>
2020-08-26 21:10:20 +0200

    set timezone

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const API_KEY = '3056be121e8647e4248094ac0a7cf18d';
 $(() => {
     const temperatureDescription = $('.temperature-description');
     const temperatureDegree = $('.temperature-degree');
-
+    const location = $('.location-timezone');
+    
     if ("geolocation" in navigator){ //check Geolocation available 
       navigator.geolocation.getCurrentPosition(position => {
           //console.log(position);
@@ -20,9 +21,12 @@ $(() => {
             .then(data => {
                 const temperature = data.main.temp;
                 const description = data.weather[0].description;
+                const country = data.sys.country;
+                const city = data.name;
 
                 temperatureDegree[0].innerHTML = temperature;
                 temperatureDescription[0].innerHTML = description;
+                location[0].innerHTML = `${country}/${city}`;
             });
       }); 
     } else{
